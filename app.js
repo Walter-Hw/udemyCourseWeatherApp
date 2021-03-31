e5aebb134bdbaec485b0b67047d534bb3734c4e9
Walter Huang <autumnhao@gmail.com>
2021-03-31 15:23:52 -0700

    Add some functions that user can input any city name.

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 //jshint esversion:6
 
-//const { response } = require('express');
 const express = require('express');
 const https = require('https');
 const { key } = require('./config');
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-  const city = `Diamond Bar`;
+app.post('/', (req, res) => {
+
+  const city = req.body.cityName;
   const key = process.env.API_KEY;
   const units = `metric`;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${units}`;
@@ -28,15 +32,19 @@ app.get('/', (req, res) => {
       const icon = weatherData.weather[0].icon;
       const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
       
-      res.write(`<h1 style="color:blue;text-align:center;">Diamond Bar City Weather</h1>`);
+      res.write(`<h1 style="color:blue;text-align:center;">${city} Weather</h1>`);
       res.write(`<h2 style="color:blue;text-align:center;">Today is ${description}.`);
       res.write(`<img src=${imageURL}>`);
-      res.write(`<h2 style="color:blue;text-align:center;">The temperature in Diamond Bar is ${temp} celsius degree.</h2>`);
+      res.write(`<h2 style="color:blue;text-align:center;">The temperature in ${city} is ${temp} celsius degree.</h2>`);
       res.send();
     });
 
   });
 });
+
+/*
+
+*/
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
